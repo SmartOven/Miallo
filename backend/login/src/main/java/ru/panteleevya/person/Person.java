873a65a04ac45d3337b5773926eedaac4ff31a38
smartoven <yarik.panteleev@gmail.com>
2023-07-29 19:30:18 +0300

    f

--- backend/login/src/main/java/ru/panteleevya/person/Person.java
+++
@@ -1,4 +0,0 @@
-package ru.panteleevya.person;
-
-public class Person {
-}
--- backend/person/src/main/java/ru/panteleevya/chat/ChatMapper.java
+++ backend/person/src/main/java/ru/panteleevya/chat/ChatMapper.java
@@ -2,8 +2,6 @@ package ru.panteleevya.chat;
 
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-
 @Service
 public class ChatMapper {
     public Chat toChat(ChatDocument chatDocument) {
