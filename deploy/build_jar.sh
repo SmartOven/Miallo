78c8f44fb05ad45a6b96874dee470941d25f67dc
smartoven <yarik.panteleev@gmail.com>
2023-08-27 20:23:28 +0300

    допиливаю скрипты

--- deploy/build_jar.sh
+++ deploy/build_jar.sh
@@ -3,5 +3,5 @@
 rm -rf ~/build/MialloBackend
 mkdir -p ~/build/MialloBackend
 cd ~/Miallo/backend
-./gradlew bootJar > /dev/null
-cp ~/Miallo/backend/build/libs/backend-1.0.0.jar ~/build/MialloBackend/backend.jar
+./gradlew bootJar
+mv ~/Miallo/backend/build/libs/backend-1.0.0.jar ~/build/MialloBackend/backend.jar
