ca0e36ba20adbf3a243cab08b7dd5fc5b5ba5708
smartoven <yarik.panteleev@gmail.com>
2023-09-02 15:00:24 +0300

    убрал двойную загрузку

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,4 @@
 import '@gravity-ui/uikit/styles/styles.css';
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/main.css'
 import App from "./components/App.tsx";
@@ -7,9 +6,7 @@ import {store} from "./features/redux/store.ts";
 import {Provider} from "react-redux";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </React.StrictMode>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
 )
